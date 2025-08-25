cb8dac6c7656524aeb6b6d17530b095e59adbfa4
Shubham Ranjan <gkr74226@gmail.com>
2025-08-26 02:39:24 +0530

    Commit...

--- src/university/management/system/Splash.java
+++ src/university/management/system/Splash.java
@@ -16,9 +16,9 @@ public class Splash extends JFrame implements Runnable{
         t.start();
         setVisible(true);
         int x = 1;
-        for(int i=2; i<=600; i+=4,x+=1){
+        for(int i=2; i<=600; i+=4, x+=1){
             setLocation(600-((i+x)/2), 350-(i/2));
-            setSize(i = 3*x, i+x/2);
+            setSize(i + 3*x, i+x/2);
             try{
                 Thread.sleep(10);
             }catch (Exception e){

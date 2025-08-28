4f6911119b483cd0acdf62193da19cfe0526eb50
Shubham Ranjan <gkr74226@gmail.com>
2025-08-29 00:52:37 +0530

    main_class

---
+++ src/university/management/system/main_class.java
@@ -0,0 +1,13 @@
+package university.management.system;
+
+import javax.swing.*;
+
+public class main_class extends JFrame {
+    main_class(){
+        setSize(500,500);
+        setVisible(true);
+    }
+    public static void main(String[] args) {
+        new main_class();
+;    }
+}

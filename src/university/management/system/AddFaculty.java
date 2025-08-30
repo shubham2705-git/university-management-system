eaeb081a479486cdf112aa8d035e58d6395ed750
Shubham Ranjan <gkr74226@gmail.com>
2025-08-31 00:34:13 +0530

    New Teachers Details....

--- src/university/management/system/AddFaculty.java
+++ src/university/management/system/AddFaculty.java
@@ -7,10 +7,11 @@ import java.awt.*;
 import java.util.Random;
 
 public class AddFaculty extends JFrame {
-    JTextField textName,textFather;
+    JTextField textName,textFather,textAddress,textPhone,textEmail,textM10,textM12,textAadhar;
     JLabel empText;
     JDateChooser cdob;
-
+    JComboBox courseBox,departmentBox;
+    JButton submit, cancel;
     Random ran = new Random();
 
     long f4 = Math.abs(ran.nextLong()%9000L) + 1000L;
@@ -24,27 +25,27 @@ public class AddFaculty extends JFrame {
         heading.setBounds(310,30,500,50);
         heading.setFont(new Font("serif",Font.BOLD,30));
         add(heading);
-
+//======================================================================================================================
         JLabel name = new JLabel("Name");
         name.setBounds(50,150,150,30);
         name.setFont(new Font("serif",Font.BOLD,20));
         add(name);
 
         textName = new JTextField();
-        textName.setBounds(200,150,200,30);
+        textName.setBounds(200,150,150,30);
         add(textName);
-
+//======================================================================================================================
         JLabel fname = new JLabel("Father Name");
-        fname.setBounds(450,150,150,30);
+        fname.setBounds(400,150,150,30);
         fname.setFont(new Font("serif",Font.BOLD,20));
         add(fname);
 
         textFather = new JTextField();
         textFather.setBounds(600,150,150,30);
         add(textFather);
-
+//======================================================================================================================
         JLabel empID = new JLabel("Employee ID");
-        empID.setBounds(50,200,150,30);
+        empID.setBounds(50,200,200,30);
         empID.setFont(new Font("serif",Font.BOLD,20));
         add(empID);
 
@@ -52,12 +53,104 @@ public class AddFaculty extends JFrame {
         empText.setBounds(200,200,150,30);
         empText.setFont(new Font("serif",Font.BOLD,20));
         add(empText);
-
+//======================================================================================================================
         JLabel dob = new JLabel("Date of Birth");
-        dob.setBounds(400,200,150,30);
+        dob.setBounds(400,200,200,30);
         dob.setFont(new Font("serif",Font.BOLD,20));
         add(dob);
 
+        cdob = new JDateChooser();
+        cdob.setBounds(600,200,150,30);
+        add(cdob);
+//======================================================================================================================
+        JLabel address = new JLabel("Address");
+        address.setBounds(50,250,200,30);
+        address.setFont(new Font("serif",Font.BOLD,20));
+        add(address);
+
+        textAddress = new JTextField();
+        textAddress.setBounds(200,250,150,30);
+        add(textAddress);
+//======================================================================================================================
+        JLabel phone = new JLabel("Phone");
+        phone.setBounds(400,250,200,30);
+        phone.setFont(new Font("serif",Font.BOLD,20));
+        add(phone);
+
+        textPhone = new JTextField();
+        textPhone.setBounds(600,250,150,30);
+        add(textPhone);
+//======================================================================================================================
+        JLabel email = new JLabel("Email");
+        email.setBounds(50,300,200,30);
+        email.setFont(new Font("serif",Font.BOLD,20));
+        add(email);
+
+        textEmail = new JTextField();
+        textEmail.setBounds(200,300,150,30);
+        add(textEmail);
+//======================================================================================================================
+        JLabel M10 = new JLabel("Class X (%)");
+        M10.setBounds(400,300,200,30);
+        M10.setFont(new Font("serif",Font.BOLD,20));
+        add(M10);
+
+        textM10 = new JTextField();
+        textM10.setBounds(600,300,150,30);
+        add(textM10);
+//======================================================================================================================
+        JLabel M12 = new JLabel("Class XII (%)");
+        M12.setBounds(50,350,200,30);
+        M12.setFont(new Font("serif",Font.BOLD,20));
+        add(M12);
+
+        textM12 = new JTextField();
+        textM12.setBounds(200,350,150,30);
+        add(textM12);
+//======================================================================================================================
+        JLabel Aadharno = new JLabel("Aadhar Number");
+        Aadharno.setBounds(400,350,200,30);
+        Aadharno.setFont(new Font("serif",Font.BOLD,20));
+        add(Aadharno);
+
+        textAadhar = new JTextField();
+        textAadhar.setBounds(600,350,150,30);
+        add(textAadhar);
+//======================================================================================================================
+        JLabel Qaulification = new JLabel("Qaulification");
+        Qaulification.setBounds(50,400,200,30);
+        Qaulification.setFont(new Font("serif",Font.BOLD,20));
+        add(Qaulification);
+
+        String[] course = {"B.Tech","BBA","BCA","BSC","MSC","MBA","MCA","MCom","MA","BA"};
+        courseBox = new JComboBox(course);
+        courseBox.setBounds(200,400,150,30);
+        courseBox.setBackground(Color.WHITE);
+        add(courseBox);
+//======================================================================================================================
+        JLabel Department = new JLabel("Department");
+        Department.setBounds(400,400,200,30);
+        Department.setFont(new Font("serif",Font.BOLD,20));
+        add(Department);
+
+        String[] department = {"Computer Science","Electronics","Mechanical","Civil","IT"};
+        departmentBox = new JComboBox(department);
+        departmentBox.setBounds(600,400,150,30);
+        departmentBox.setBackground(Color.WHITE);
+        add(departmentBox);
+//======================================================================================================================
+        submit = new JButton("Submit");
+        submit.setBounds(250,550,120,30);
+        submit.setBackground(Color.BLACK);
+        submit.setForeground(Color.WHITE);
+        add(submit);
+
+        cancel = new JButton("Cancel");
+        cancel.setBounds(450,550,120,30);
+        cancel.setBackground(Color.BLACK);
+        cancel.setForeground(Color.WHITE);
+        add(cancel);
+//======================================================================================================================
 
 
 

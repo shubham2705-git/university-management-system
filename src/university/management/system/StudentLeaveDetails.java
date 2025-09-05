5846b2764b81c9ce6c4f81f0214ec9484ad3f959
Shubham Ranjan <gkr74226@gmail.com>
2025-09-05 23:58:18 +0530

    StudentLeaveDetails

---
+++ src/university/management/system/StudentLeaveDetails.java
@@ -0,0 +1,21 @@
+package university.management.system;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class StudentLeaveDetails extends JFrame implements ActionListener {
+
+    StudentLeaveDetails(){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public static void main(String[] args) {
+        new StudentLeaveDetails();
+    }
+}
